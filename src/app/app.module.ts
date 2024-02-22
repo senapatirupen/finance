133c311f5f096cd6c80ef813f0c1825cc7380436
senapatirupen <senapatirupen1986@gmail.com>
2024-02-22 12:44:21 +0530

    added EW index fund, SGB and Liquid Fund investment strategy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FinanceComponent } from './finance/finance.component';
@@ -45,6 +45,7 @@ import { FormsModule } from '@angular/forms';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     HttpClientModule,
     FormsModule
--- src/app/finance/finance.component.ts
+++ src/app/finance/finance.component.ts
@@ -1,10 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { Component, OnInit, HostListener } from '@angular/core';
 import 'bootstrap/dist/js/bootstrap.min.js';
 
 @Component({
   selector: 'app-finance',
   templateUrl: './finance.component.html',
-  styleUrls: ['./finance.component.scss']
+  styleUrls: ['./finance.component.scss'],
+  animations: [
+    trigger('hoverEffect', [
+      state('normal', style({
+        transform: 'scale(1)',
+      })),
+      state('enlarged', style({
+        transform: 'scale(1.2)',
+      })),
+      transition('normal <=> enlarged', animate('200ms ease-in-out')),
+    ]),
+  ]
 })
 export class FinanceComponent implements OnInit {
   externalLink: string = '';
@@ -12,23 +24,38 @@ export class FinanceComponent implements OnInit {
   expectedReturnRate: number = 0;
   investmentTenure: number = 0;
   futureValue: number = 0;
+  totalInvestedAmount = 0;
 
   loanAmount: number = 0;
   interestRate: number = 0;
   loanTenure: number = 0;
-  emi: number =0;
+  emi: number = 0;
   totalInterestPaid: number = 0;
   totalAmountPaid: number = 0;
+
+  annualRateOfReturn: number = 0;
+  yearsToDouble: number = 0;
+
+  principalAmount: number = 0;
+  finalAmount: number = 0;
+  numberOfYears: number = 0;
+  cagr: number = 0;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  hoverState1 = 'normal';
+
+  toggleHoverState() {
+    this.hoverState1 = (this.hoverState1 === 'normal' ? 'enlarged' : 'normal');
+  }
+
   calculateFutureValue(): void {
     const monthlyRate = this.expectedReturnRate / 12 / 100;
     const totalMonths = this.investmentTenure * 12;
-
+    this.totalInvestedAmount = this.investmentAmount! * totalMonths;
     this.futureValue = this.investmentAmount *
       (((Math.pow(1 + monthlyRate, totalMonths) - 1) / monthlyRate) * (1 + monthlyRate));
   }
@@ -36,11 +63,29 @@ export class FinanceComponent implements OnInit {
   calculateEmi(): void {
     const monthlyRate = (this.interestRate! / 12) / 100;
     const totalMonths = this.loanTenure! * 12;
-    
+
     this.emi = (this.loanAmount! * monthlyRate) / (1 - Math.pow(1 + monthlyRate, -totalMonths));
     this.totalAmountPaid = this.emi! * totalMonths;
     this.totalInterestPaid = this.totalAmountPaid! - this.loanAmount!;
-  
+
+  }
+
+  // Function to calculate the number of years to double using the Rule of 72
+  calculateYearsToDouble(): void {
+    // Ensure the interest rate is a valid positive number
+    if (this.annualRateOfReturn <= 0) {
+      alert("Interest rate must be a positive number");
+      return;
+    }
+    // Calculate the number of years using the Rule of 72
+    this.yearsToDouble = 72 / this.annualRateOfReturn;
+  }
+
+  calculateCAGR(): void {
+    const numerator = this.finalAmount / this.principalAmount;
+    const power = 1 / this.numberOfYears;
+    this.cagr = Math.pow(numerator, power) - 1;
+    this.cagr = this.cagr * 100; // Convert to percentage
   }
 
   resetSipForm(): void {
@@ -48,6 +93,7 @@ export class FinanceComponent implements OnInit {
     this.expectedReturnRate = 0;
     this.investmentTenure = 0;
     this.futureValue = 0;
+    this.totalInvestedAmount = 0;
   }
 
   resetEmiForm(): void {
@@ -59,9 +105,32 @@ export class FinanceComponent implements OnInit {
     this.totalAmountPaid = 0;
   }
 
+  resetCagrForm(): void {
+    this.principalAmount = 0;
+    this.finalAmount = 0;
+    this.numberOfYears = 0;
+    this.cagr = 0;
+  }
+
   openLinkInNewTab(externalLink: string) {
     this.externalLink = externalLink;
     window.open(this.externalLink, '_blank');
+  }
+
+
+  shouldShow: boolean = false;
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.shouldShow = window.scrollY > 100; // Adjust this value based on when you want the button to appear
+  }
+
+  scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
+
+  shouldShowButton(): boolean {
+    return this.shouldShow;
   }
 
 }
